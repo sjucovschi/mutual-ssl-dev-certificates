cd833b16cedb820e6abc47d43d851a00de6cf605
Sorin Jucovschi <jucosorin@gmail.com>
2021-08-30 17:52:51 +0300

    Add generated certificates

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-#### Generate certificates for testing MSSL in the Orian project
+#### Generate certificates for testing 2 way TLS
 
-This project creates all files needed for enabling Mutual SSL configuration in the Orian project.
+This project creates all files needed for enabling testing of a 2 way TLS setup (Mutual SSL).
 The client and sever certificates are created by using a root certificate authority. The **_ca-cert.pem_** certificate
 is used to validate the identity of the two. 
 
 The **_domains.ext_** files define the domain names that these certificates are valid for. I've included **_host.docker.internal_**,
 besides localhost, so that MSSL and SSL requests could work between two docker hosts.
 
-The provided script should be run within WSL for Windows or in a Linux Docker container. Some of the openssl commands **do
-not work directly under Windows 10**. 
-
-The certificates to use will be copied over to the dist folder. They are named accordingly. 
+The certificates to use will be copied over to the dist folder.
 
 The folder named **_postman_**, inside the **_dist_** folder, contains certificates that enable testing through Postman. 
 Go to Postman/Settings/Certificates and add them there.
--- two-way-authentication-by-trusting-root.ca.sh
+++ two-way-authentication-by-trusting-root.ca.sh
@@ -8,22 +8,21 @@ cleanUpExistingCertificatesAndKeystores() {
     rm -fv client/client-cert.pem
     rm -fv client/client.p12
     rm -fv client/keystore.jks
-    rm -fv client/truststore.jks
-  
+
     rm -fv ca/ca-cert.pem
     rm -fv ca/ca-key.pem
     rm -fv ca/ca-cert.srl
+    rm -fv ca/truststore.jks
 
     rm -fv server/server-key.pem
     rm -fv server/server.csr
     rm -fv server/server-cert.pem
-    rm -fv server/server-cer-including-private-key.pem
+    rm -fv server/server-cert-including-private-key.pem
+    rm -fv server/server.p12
+    rm -fv server/keystore.jks
 
-    rm -fv dist/ssl_pps-edenred_chain.pem
-    rm -fv dist/ssl_pps-int_chain.pem
-    rm -fv dist/ssl_pps-int_full_chain.pem
-    rm -fv dist/ssl_pps-int_key.pem
-    rm -fv dist/keystore.jks
+    rm -fv dist/client-keystore.jks
+    rm -fv dist/server-keystore.jks
     rm -fv dist/truststore.jks
  
     rm -r dist/postman
@@ -36,13 +35,13 @@ createCertificates() {
     echo 'Starting to create certificates...'
 
     # generate the private key and the certificate for the certificate authority (CA); private key with no passphrase (-nodes)
-    openssl req -nodes -x509 -newkey rsa:4096 -keyout ca/ca-key.pem -out ca/ca-cert.pem -days 3650  -subj "/C=UK/ST=Wiltshire/L=Swindon/O=Prepay Solutions/OU=IT Department/CN=PPS Certificate Authority"
+    openssl req -nodes -x509 -newkey rsa:4096 -keyout ca/ca-key.pem -out ca/ca-cert.pem -days 3650  -subj "/C=RO/ST=Bucharest/L=Bucharest/O=Jucosystems/OU=IT Department/CN=Jucosystems Certificate Authority"
 
     #generate the server private key and server certificate signing request (CSR); private key with no passphrase (-nodes)
-    openssl req  -nodes -newkey rsa:4096 -keyout server/server-key.pem -out server/server.csr -subj "/C=UK/ST=Wiltshire/L=Swindon/O=Service Test/OU=IT Department/CN=pps-client.com"
+    openssl req  -nodes -newkey rsa:4096 -keyout server/server-key.pem -out server/server.csr -subj "/C=RO/ST=RO/L=Bucharest/O=Client Service/OU=IT Department/CN=client-service.com"
 
     #generate the client private key and client certificate signing request (CSR); private key with no passphrase (-nodes)
-    openssl req -nodes -newkey rsa:4096 -keyout client/client-key.pem -out client/client.csr -subj "/C=UK/ST=Wiltshire/L=Swindon/O=Prepay Solutions/OU=IT Department/CN=pps-int.com"
+    openssl req -nodes -newkey rsa:4096 -keyout client/client-key.pem -out client/client.csr -subj "/C=RO/ST=Bucharest/L=Bucharest/O=Server Service/OU=IT Department/CN=server-service.com"
 
     #sign the server certificate with the CA
     openssl x509 -req -days 3650 -in server/server.csr -CA ca/ca-cert.pem -CAkey ca/ca-key.pem -CAcreateserial -extfile server/domains.ext -out server/server-cert.pem
@@ -51,7 +50,7 @@ createCertificates() {
     openssl x509 -req -days 3650 -in client/client.csr -CA ca/ca-cert.pem -CAkey ca/ca-key.pem -CAcreateserial -extfile client/domains.ext -out client/client-cert.pem
 
     #add the server private key to the server certificate for the haproxy config
-    cat server/server-cert.pem server/server-key.pem > server/server-cer-including-private-key.pem
+    cat server/server-cert.pem server/server-key.pem > server/server-cert-including-private-key.pem
 
     echo 'Verify the client certificate...'
     openssl verify -CAfile ca/ca-cert.pem client/client-cert.pem
@@ -62,25 +61,27 @@ createCertificates() {
 
 createClientKeyStoreAndTrustStore() {
 
-    #convert the client cert and secret key into a pkcs12 file (mandatory since keytool cannot just import a cert + private key directly); must set a password because step 2 fails otherwise
+    #convert the client cert and secret key into a pkcs12 file (mandatory since keytool cannot just import a cert + private key directly); must set a password because next step fails otherwise
     openssl pkcs12 -export -in client/client-cert.pem -inkey client/client-key.pem -out client/client.p12 -name client -CAfile ca/ca-cert.pem -chain -passout pass:secret
 
-    #create a java keystore from the pkcs12 file; this will contain the client certificate, encrypted client private key, and the certificate authority (because we added the -chain option for the previous command)
+    #create a java keystore for the client service from the pkcs12 file; this will contain the client certificate, encrypted client private key, and the certificate authority (because we added the -chain option for the previous command)
     keytool -importkeystore -deststorepass secret -destkeypass secret -destkeystore client/keystore.jks -srckeystore client/client.p12 -srcstoretype JKS -srcstorepass secret -alias client
 
+    #convert the server cert and secret key into a pkcs12 file (mandatory since keytool cannot just import a cert + private key directly); must set a password because next step fails otherwise
+    openssl pkcs12 -export -in server/server-cert.pem -inkey server/server-key.pem -out server/server.p12 -name server -CAfile ca/ca-cert.pem -chain -passout pass:secret
+
+    #create a java keystore for the server service from the pkcs12 file; this will contain the server certificate, encrypted server private key, and the certificate authority (because we added the -chain option for the previous command)
+    keytool -importkeystore -deststorepass secret -destkeypass secret -destkeystore server/keystore.jks -srckeystore server/server.p12 -srcstoretype JKS -srcstorepass secret -alias server
+
     #create a java truststore and import the certificate authority certificate into it (this will validate any certificates signed by the certificate authority)
-    keytool -keystore client/truststore.jks -importcert -file ca/ca-cert.pem -alias root-ca -storepass secret -noprompt
+    keytool -keystore ca/truststore.jks -importcert -file ca/ca-cert.pem -alias root-ca -storepass secret -noprompt
 }
 
-renameCertificatesToMatchExistingNames() {
-    cp ca/ca-cert.pem dist/ssl_pps-edenred_chain.pem
+createDist() {
 
-    cp server/server-cert.pem dist/ssl_pps-int_chain.pem
-    cp server/server-cer-including-private-key.pem dist/ssl_pps-int_full_chain.pem
-    cp server/server-key.pem dist/ssl_pps-int_key.pem
-
-    cp client/keystore.jks dist/keystore.jks
-    cp client/truststore.jks dist/truststore.jks
+    cp client/keystore.jks dist/client-keystore.jks
+    cp server/keystore.jks dist/server-keystore.jks
+    cp ca/truststore.jks dist/truststore.jks
 
     mkdir dist/postman
 
@@ -92,4 +93,4 @@ renameCertificatesToMatchExistingNames() {
 cleanUpExistingCertificatesAndKeystores
 createCertificates
 createClientKeyStoreAndTrustStore
-renameCertificatesToMatchExistingNames
+createDist
